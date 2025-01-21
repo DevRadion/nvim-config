b2cc1e41469f1e7e50d09bc66a652b2311b256f2
Radion Rusnak <radion.rusnak@gmail.com>
2025-01-21 18:40:19 +0200

    Added conform plugin minimal configuration and clang-format

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,6 +1,15 @@
 return {
     {
         "olimorris/onedarkpro.nvim",
-        priority = 1000, -- Ensure it loads first
+        priority = 1000,
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                c = { "clang-format" },
+                go = { "gofmt" },
+            },
+        },
     },
 }
