737b02a7f2cb079304b62bfda9c38e8e822fd4ab
Radion Rusnak <radion.rusnak@gmail.com>
2025-03-04 17:42:38 +0200

    Changed lazy.nvim setup

--- init.lua
+++ init.lua
@@ -24,6 +24,30 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 	},
-	-- automatically check for plugin updates
-	checker = { enabled = true },
+	defaults = {
+		lazy = false,
+		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+		-- have outdated releases, which may break your Neovim install.
+		version = false, -- always use the latest git commit
+		-- version = "*", -- try installing the latest stable version for plugins that support semver
+	},
+	checker = {
+		enabled = true, -- check for plugin updates periodically
+		notify = false, -- notify on update
+	}, -- automatically check for plugin updates
+	performance = {
+		rtp = {
+			-- disable some rtp plugins
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				-- "netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
