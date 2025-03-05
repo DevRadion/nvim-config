37ab91aa53b491ea5811543bfc8e33096d20e2a4
Radion Rusnak <radion.rusnak@gmail.com>
2025-03-05 17:51:47 +0200

    Added some mini modules and color highlighting, removed nvim.autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -31,7 +31,8 @@ return {
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
 			require("mini.surround").setup()
-			require("mini.starter").setup()
+			require("mini.pairs").setup()
+			require("mini.indentscope").setup()
 
 			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = vim.g.have_nerd_font })
