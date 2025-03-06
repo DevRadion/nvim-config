383daa75047b66af1bae3e6e14502cb9087f25a0
Radion Rusnak <radion.rusnak@gmail.com>
2025-03-06 09:41:19 +0200

    Refactored lspconfig setup and added zig.vim plugin

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -4,3 +4,9 @@ vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true
 
 vim.g.autocomplete_max_item_count = 20
+
+-- don't show parse errors in a separate window
+vim.g.zig_fmt_parse_errors = 0
+
+-- disable format-on-save from `ziglang/zig.vim`
+vim.g.zig_fmt_autosave = 0
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -4,7 +4,11 @@ return {
 	{
 		"rcarriga/nvim-notify",
 		config = function()
-			vim.notify = require("notify")
+			local notify = require("notify")
+			notify.setup({
+				background_colour = "#000000",
+			})
+			vim.notify = notify
 		end,
 	},
 }
