74b6ecf9cdb65f0fcfd3a6648e47edf6e8eabd61
Radion Rusnak <radion.rusnak@gmail.com>
2025-05-26 11:33:03 +0300

    [main] Updated rustaceanvim configuration

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,4 +2,30 @@ return {
 	"mrcjkb/rustaceanvim",
 	version = "^6", -- Recommended
 	lazy = false, -- This plugin is already lazy
+	config = function()
+		vim.g.rustaceanvim = {
+			-- Plugin configuration
+			tools = {},
+
+			-- LSP configuration
+			server = {
+				default_settings = {
+					-- rust-analyzer language server configuration
+					["rust-analyzer"] = {
+						checkOnSave = true,
+						diagnostics = {
+							styleLints = {
+								enable = true,
+							},
+						},
+						cargo = {
+							features = "all",
+						},
+					},
+				},
+			},
+			-- DAP configuration
+			dap = {},
+		}
+	end,
 }
